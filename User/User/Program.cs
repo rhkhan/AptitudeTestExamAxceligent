77ca93ba14ab7479d1a87bba8d1124d504174250
unknown <r.hossain@rashed-PC.cimsolutionsbd.local>
2018-10-12 18:09:12 +0600

    First Commit

---
+++ User/User/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserClass
+{
+    class User
+    {
+        public static List<string> users = new List<string>();
+
+        static void Add(string username)
+        {
+            users.Add(username);
+        }
+
+        static int GetUsersCount()
+        {
+            return users.Count();
+        }
+    }
+
+}
